85f8c5e33f0951427bccab004818e83990855c04
Emilien Coquard <github@emln.co>
2013-10-08 15:09:11 +0200

    Fix truncated date regex

--- src/validation/Date.php
+++ src/validation/Date.php
@@ -10,7 +10,7 @@ class Date extends \Validator\Validation_Abstract
 {
 	public function check($value)
 	{
-		$pattern	=	'#^(?P<year>(?:19|20)\d{2})-(?P<month>[0]\d|[1][0-2])-(?P<day>[0-2]\d|[3][0-1])';
+		$pattern	=	'#^(?P<year>(?:19|20)\d{2})-(?P<month>[0]\d|[1][0-2])-(?P<day>[0-2]\d|[3][0-1])$#';
 
 		if( ! preg_match($pattern, $value, $parts) || ! checkdate($parts['month'], $parts['day'], $parts['year']))
 			throw new \Exception(\Validator\i18n::get('error_date'));
